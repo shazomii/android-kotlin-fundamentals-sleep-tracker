4e5f728eeca97d64d80a2de17460b5f0db687fdb
shazomii <seyeoyeniran2u@gmail.com>
2020-07-24 22:02:39 +0100

    Refresh list content with DiffUtil

--- app/src/main/java/com/davenet/sleeptracker/sleeptracker/SleepNightAdapter.kt
+++ app/src/main/java/com/davenet/sleeptracker/sleeptracker/SleepNightAdapter.kt
@@ -5,31 +5,24 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
 import com.davenet.sleeptracker.R
 import com.davenet.sleeptracker.convertDurationToFormatted
 import com.davenet.sleeptracker.convertNumericQualityToString
 import com.davenet.sleeptracker.database.SleepNight
 
-class SleepNightAdapter : RecyclerView.Adapter<SleepNightAdapter.ViewHolder>() {
-    var data = listOf<SleepNight>()
-        set(value) {
-            field = value
-            notifyDataSetChanged()
-        }
+class SleepNightAdapter : ListAdapter<SleepNight, SleepNightAdapter.ViewHolder>(SleepNightDiffCallback()) {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         return ViewHolder.from(parent)
     }
 
-    override fun getItemCount(): Int = data.size
-
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        val item = data[position]
+        val item = getItem(position)
         holder.bind(item)
-
     }
-
 
     class ViewHolder private constructor(itemView: View) : RecyclerView.ViewHolder(itemView) {
         private val sleepLength: TextView = itemView.findViewById(R.id.sleep_length)
@@ -64,6 +57,14 @@ class SleepNightAdapter : RecyclerView.Adapter<SleepNightAdapter.ViewHolder>() {
             }
         }
     }
+}
 
+class SleepNightDiffCallback: DiffUtil.ItemCallback<SleepNight>() {
+    override fun areItemsTheSame(oldItem: SleepNight, newItem: SleepNight): Boolean {
+        return oldItem.nightId == newItem.nightId
+    }
 
+    override fun areContentsTheSame(oldItem: SleepNight, newItem: SleepNight): Boolean {
+        return oldItem == newItem
+    }
 }
--- app/src/main/java/com/davenet/sleeptracker/sleeptracker/SleepTrackerFragment.kt
+++ app/src/main/java/com/davenet/sleeptracker/sleeptracker/SleepTrackerFragment.kt
@@ -62,7 +62,7 @@ class SleepTrackerFragment : Fragment() {
             })
             nights.observe(viewLifecycleOwner, Observer {
                 it?.let {
-                    adapter.data = it
+                    adapter.submitList(it)
                 }
             })
         }

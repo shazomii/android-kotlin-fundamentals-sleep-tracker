1fb0ac5c6632a6dc9faf679eea6f51511a73f1f6
shazomii <seyeoyeniran2u@gmail.com>
2020-07-25 09:38:54 +0100

    Introduce binding adapters

---
+++ app/src/main/java/com/davenet/sleeptracker/sleeptracker/BindingUtils.kt
@@ -0,0 +1,33 @@
+package com.davenet.sleeptracker.sleeptracker
+
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.databinding.BindingAdapter
+import com.davenet.sleeptracker.R
+import com.davenet.sleeptracker.convertDurationToFormatted
+import com.davenet.sleeptracker.convertNumericQualityToString
+import com.davenet.sleeptracker.database.SleepNight
+
+@BindingAdapter("sleepDurationFormatted")
+fun TextView.setSleepDurationFormatted(item: SleepNight) {
+    text = convertDurationToFormatted(item.startTimeMilli, item.endTimeMilli, context.resources)
+}
+
+@BindingAdapter("sleepQualityString")
+fun TextView.setSleepQualityString(item: SleepNight) {
+    text = convertNumericQualityToString(item.sleepQuality, context.resources)
+}
+
+@BindingAdapter("sleepImage")
+fun ImageView.setSleepImage(item: SleepNight) {
+    setImageResource(when (item.sleepQuality) {
+            0 -> R.drawable.ic_sleep_0
+            1 -> R.drawable.ic_sleep_1
+            2 -> R.drawable.ic_sleep_2
+            3 -> R.drawable.ic_sleep_3
+            4 -> R.drawable.ic_sleep_4
+            5 -> R.drawable.ic_sleep_5
+            else -> R.drawable.ic_sleep_active
+        }
+    )
+}
--- app/src/main/java/com/davenet/sleeptracker/sleeptracker/SleepNightAdapter.kt
+++ app/src/main/java/com/davenet/sleeptracker/sleeptracker/SleepNightAdapter.kt
@@ -13,7 +13,8 @@ import com.davenet.sleeptracker.convertNumericQualityToString
 import com.davenet.sleeptracker.database.SleepNight
 import com.davenet.sleeptracker.databinding.ListItemSleepNightBinding
 
-class SleepNightAdapter : ListAdapter<SleepNight, SleepNightAdapter.ViewHolder>(SleepNightDiffCallback()) {
+class SleepNightAdapter :
+    ListAdapter<SleepNight, SleepNightAdapter.ViewHolder>(SleepNightDiffCallback()) {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         return ViewHolder.from(parent)
@@ -24,27 +25,12 @@ class SleepNightAdapter : ListAdapter<SleepNight, SleepNightAdapter.ViewHolder>(
         holder.bind(item)
     }
 
-    class ViewHolder private constructor(val binding: ListItemSleepNightBinding) : RecyclerView.ViewHolder(binding.root) {
-        fun bind(
-            item: SleepNight
-        ) {
-            val res = itemView.context.resources
-
+    class ViewHolder private constructor(val binding: ListItemSleepNightBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+        fun bind(item: SleepNight) {
             binding.apply {
-                sleepLength.text =
-                    convertDurationToFormatted(item.startTimeMilli, item.endTimeMilli, res)
-                qualityString.text = convertNumericQualityToString(item.sleepQuality, res)
-                qualityImage.setImageResource(
-                    when (item.sleepQuality) {
-                        0 -> R.drawable.ic_sleep_0
-                        1 -> R.drawable.ic_sleep_1
-                        2 -> R.drawable.ic_sleep_2
-                        3 -> R.drawable.ic_sleep_3
-                        4 -> R.drawable.ic_sleep_4
-                        5 -> R.drawable.ic_sleep_5
-                        else -> R.drawable.ic_sleep_active
-                    }
-                )
+                sleep = item
+                executePendingBindings()
             }
         }
 
@@ -58,7 +44,7 @@ class SleepNightAdapter : ListAdapter<SleepNight, SleepNightAdapter.ViewHolder>(
     }
 }
 
-class SleepNightDiffCallback: DiffUtil.ItemCallback<SleepNight>() {
+class SleepNightDiffCallback : DiffUtil.ItemCallback<SleepNight>() {
     override fun areItemsTheSame(oldItem: SleepNight, newItem: SleepNight): Boolean {
         return oldItem.nightId == newItem.nightId
     }
